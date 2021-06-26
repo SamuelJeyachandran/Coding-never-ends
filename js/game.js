3491a75b2e7e2a37d85758a41cafe71084440f30
Samuel Jeyachandran <david.jeyachandran@sim.org>
2021-06-26 12:58:46 -0500

    Made game (not finished)

---
+++ js/game.js
@@ -0,0 +1,30 @@
+(function(){    
+    let num = Math.floor(Math.random() * (100 - 1)) + 1;
+    console.log(num)
+    function checkNum(event){
+    
+        const inputField = document.getElementById('input')
+        const input = parseInt(inputField.value)
+        console.log(input)
+        if(input === num){
+            console.log("Congratulations that's the right number")
+        }
+        else if(input > num){
+            console.log("too high")
+        }
+        else if(input < num){
+            console.log("too low")
+        }
+        else{
+            console.log("Oops something is broken")
+        }
+
+        // clear field value
+        inputField.value = ""
+        event.preventDefault();
+    }
+
+    const form = document.getElementById('form');
+    form.addEventListener('submit', checkNum);
+
+})()
