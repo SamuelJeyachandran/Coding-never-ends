19f2f7c1579a7a465b48d71c819266d9ba7bb22f
Samuel Jeyachandran <david.jeyachandran@sim.org>
2021-06-28 17:23:11 -0500

    Made game visible and play again button

--- js/game.js
+++ js/game.js
@@ -1,27 +1,36 @@
 (function(){    
     let num = Math.floor(Math.random() * (100 - 1)) + 1;
-    console.log(num)
+    let response = ''
+    let howManyTurns = 0
+    let s = ''
     function checkNum(event){
     
         const inputField = document.getElementById('inputnum')
         const input = parseInt(inputField.value)
-        console.log(input)
         if(input === num){
-            console.log("Congratulations that's the right number")
+            response = "Congratulations that's the right number";
+            howManyTurns++;
+            document.getElementById('form').hidden=true
+            document.getElementById("turn").innerHTML = `You have had ${howManyTurns} turn${s}`
+            document.getElementById('hButton').hidden=false
         }
         else if(input > num){
-            console.log("too high")
+            response = "too high";
+            howManyTurns++;
         }
         else if(input < num){
-            console.log("too low")
+            response = "too low";
+            howManyTurns++;
         }
         else{
-            console.log("Oops something is broken")
+            response.replace("Oops something is broken")
         }
 
         // clear field value
         inputField.value = ""
         event.preventDefault();
+        document.getElementById("answer").innerHTML = response
+        s = 's'
     }
 
     const form = document.getElementById('form');
