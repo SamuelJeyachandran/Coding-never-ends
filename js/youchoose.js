64d9116d29799d238d16c688900ba234f309baa8
Samuel Jeyachandran <david.jeyachandran@sim.org>
2022-06-24 21:34:11 +1000

    Added Play Again and Back Buttons

--- js/youchoose.js
+++ js/youchoose.js
@@ -1,26 +1,23 @@
 const start = document.getElementById("start");
-const buttonA = document.getElementById("buttonA");
-const buttonB = document.getElementById("buttonB");
-const buttonC = document.getElementById("buttonC");
+const passageway1 = document.getElementById("passageway1");
+const again = document.getElementById("again");
+const hback = document.getElementById("back");
+
 function startYouChoose() {
   start.hidden = true;
-  buttonA.hidden = false;
-  buttonB.hidden = false;
-  buttonC.hidden = false;
+  passageway1.hidden = false;
+  again.hidden = false;
+  hback.hidden = false;
 }
 function activate(id) {
-  buttonA.hidden = true;
-  buttonB.hidden = true;
-  buttonC.hidden = true;
+  passageway1.hidden = true;
   const text = document.getElementById("text" + id);
   const text2 = document.getElementById("text2" + id);
   text.hidden = false;
   text2.hidden = false;
 }
 function replay() {
-  buttonA.hidden = true;
-  buttonB.hidden = true;
-  buttonC.hidden = true;
+  passageway1.hidden = true;
   start.hidden = false;
   textA.hidden = true;
   textB.hidden = true;
@@ -28,4 +25,24 @@ function replay() {
   text2A.hidden = true;
   text2B.hidden = true;
   text2C.hidden = true;
+  again.hidden = true;
+  hback.hidden = true;
+}
+function back() {
+  if (passageway1.hidden === true) {
+    passageway1.hidden = false;
+    textA.hidden = true;
+    textB.hidden = true;
+    textC.hidden = true;
+    text2A.hidden = true;
+    text2B.hidden = true;
+    text2C.hidden = true;
+    return;
+  }
+  if ((start.hidden = true)) {
+    again.hidden = true;
+    hback.hidden = true;
+    start.hidden = false;
+    passageway1.hidden = true;
+  }
 }
